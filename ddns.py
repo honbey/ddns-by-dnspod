c0ca2755a203687092b67ea3f03babefa0dc3f1a
honbey <honbey@qq.com>
2025-05-31 20:05:47 +0800

    perf: push notify to Gotify when IP changed

--- ddns.py
+++ ddns.py
@@ -2,58 +2,68 @@
 # _*_ coding:utf-8 _*_
 #
 
+import argparse
 import logging
-import os
+
+import yaml
 
 from db import DomainDatabase
 from dnspod import DNSPodAPI
 from iptools import IpInfo
-from utils import Logger
-
-LOG_LEVEL = logging.INFO
-
-logger = Logger("DDNS by DNSPod", level=LOG_LEVEL)
+from utils import Logger, push2gotify
 
 
-def ddns() -> bool:
-    key = (
-        os.environ.get("TENCENTCLOUD_API_PUB_KEY"),
-        os.environ.get("TENCENTCLOUD_API_PRI_KEY"),
-    )
-    db = DomainDatabase(
-        db="/opt/data/workspace/ddns-by-dnspod/data.db", log_level=LOG_LEVEL
-    )
+def ddns(config: str, force_update: bool = False) -> bool:
+    with open(config, "r") as f:
+        cfg = yaml.safe_load(f)
+        database = cfg.get("database")
+        ip_config = cfg.get("ip_config")
+        gotify = cfg.get("gotify")
+    log_level = logging.INFO
+    logger = Logger("DDNS by DNSPod", level=log_level)
+    db = DomainDatabase(db=database, log_level=log_level)
     record_list = db.query_record_by_group("DDNS")
     one_record = record_list[0]
     record_ip = one_record.get("value", "0.0.0.0")
     domain = one_record.get("domain", "example.com")
     subdomain = one_record.get("subdomain", "@")
     fulldomain = domain if subdomain == "@" else subdomain + "." + domain
-    info = IpInfo(config="/opt/data/workspace/ddns-by-dnspod/ip.yaml")
+    info = IpInfo(config=ip_config)
     url_ip = str(info.get_ip())
     dns_ip = str(info.dns_resolve(fulldomain))
+    if force_update:
+        record_ip, dns_ip = ["", ""]
     logger.info(f"URL IP: [{url_ip}], DNS IP: [{dns_ip}], record IP: [{record_ip}]")
     if IpInfo.judge_ip(url_ip) and url_ip == dns_ip:
-        logger.info("URL IP is equal to DNS IP, skipping ddns")
+        logger.info("URL IP is equal to DNS IP, skipping DDNS.")
         return False
     elif IpInfo.judge_ip(url_ip) and url_ip == record_ip:
-        logger.info("URL IP is equal to record IP, skipping ddns")
+        logger.info("URL IP is equal to record IP, skipping DDNS.")
         return False
     else:
-        dnspod = DNSPodAPI(key, log_level=LOG_LEVEL)  # type: ignore
+        dnspod = DNSPodAPI(db.key, log_level=log_level)
+        msg = ""
         for record in record_list:
+            subdomain = record.get("subdomain", "@")
+            record_id = record.get("record_id", 0)
+            fulldomain = domain if subdomain == "@" else subdomain + "." + domain
             dnspod.modify_ddns_record(
                 {
                     "Domain": domain,
-                    "SubDomain": record.get("subdomain"),
-                    "RecordId": record.get("record_id"),
+                    "SubDomain": subdomain,
+                    "RecordId": record_id,
                     "RecordLine": "默认",
                 }
             )
+            msg = f"Successfully updated {record_ip} to {url_ip} for {fulldomain}."
+            logger.info(msg)
             db.update_record_value(record.get("record_id", 0), url_ip)
-        logger.info(f"Successfully updated {record_ip} to {url_ip}.")
+        push2gotify("DDNS by DNSPod", msg, gotify.get("url"), gotify.get("token"))
         return True
 
 
 if __name__ == "__main__":
-    ddns()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", type=str, help="The path of config file")
+    args = parser.parse_args()
+    ddns(args.config, force_update=True)
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import logging
 from typing import Literal
 
+import requests
+
 
 def first(n):
     return n[0] if isinstance(n, list) else n
@@ -18,8 +20,35 @@ def Logger(
     else:
         fmt = "%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s() - %(message)s"
     logging.basicConfig(
-        level=level,
         format=fmt,
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    return logging.getLogger(name)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    return logger
+
+
+def push2gotify(title: str, msg: str, url: str, token: str, priority: int = 5):
+    """
+    Push notification to Gotify.
+
+    Params:
+    title (str): notification title
+    message (str): notification message
+    url (str): Gotify server URL
+    token (str): Gotify token
+    priority (int): notification priority
+    """
+
+    url = f"{url}/message"
+    headers = {"X-Gotify-Key": token, "Content-Type": "application/json"}
+    data = {"title": title, "message": msg, "priority": priority}
+
+    logger = Logger("Gotify")
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        logger.info("Push update notification to Gotify...")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Push notification to Gotify failed. error msg: {e}")
